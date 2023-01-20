ffac91b0e75ff794ceabb501b12896514337a1c6
owltz <>
2023-01-20 18:31:51 -0300

    generates a fake file hash

---
+++ src/Pls.ts
@@ -0,0 +1,5 @@
+export class Pls {
+  generateFileHash(filePath: string) {
+    return 'fake-hash'
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { Pls } from './PLS'
+import { PlsUser } from './PlsUser'
+
 console.log('starting...')
+const pls = new Pls()
+
+// 2.3.1.2.a
+const filePath = './sample.pdf'
+const fileHash = pls.generateFileHash(filePath)
+console.log('fileHash', fileHash)
+
+// 4.1. Use the Bitcoin Testnet to create the multisig wallet (sec- tion 2.3.1, item 2)
